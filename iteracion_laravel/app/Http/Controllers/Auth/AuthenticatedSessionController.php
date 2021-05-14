972f647e18190fd534ce61b5c73b289a3f64e19b
Jose Maria Farfan <magoalakai@gmail.com>
2021-05-14 14:20:23 +0200

    Breeze, JWT instalado y registro correcto

---
+++ iteracion_laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use App\Models\User;
+use Validator;
+
+class AuthenticatedSessionController extends Controller
+{
+
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(LoginRequest $request)
+    {
+        $request->authenticate();
+        //$request->session()->regenerate();
+
+        $credentials = $request->only('email', 'password');
+        $token = JWTAuth::attempt($credentials);
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        if($token){
+            return response()->json([
+                'success' => true,
+                'token' => $token,
+                'user' => User::where('email', $credentials['email'])->get()->first(),
+            ], status:200);
+        }else{
+            return response()->json([
+                'success' => false,
+                'message' => 'Wrong credentials',
+                'errors' => $validator->errors(),
+            ], status:401);
+        }
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- iteracion_laravel/routes/api.php
+++ iteracion_laravel/routes/api.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+//Clients & Employees
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\EmployeeController;
+
+//Auth
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
@@ -27,9 +30,17 @@ Route::get('/users/clients/{id}', [ClientController::class, 'show']);
 Route::put('/users/clients/{id}', [ClientController::class, 'update']);
 Route::delete('/users/clients/{id}', [ClientController::class, 'destroy']);
 
+
 //CRUD de Employee
 Route::get('/users/employees', [EmployeeController::class, 'index']);
 Route::post('/users/employees', [EmployeeController::class, 'store']);
 Route::get('/users/employees/{id}', [EmployeeController::class, 'show']);
 Route::put('/users/employees/{id}', [EmployeeController::class, 'update']);
 Route::delete('/users/employees/{id}', [EmployeeController::class, 'destroy']);
+
+
+//AUTH
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::get('/auth/logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->middleware('jwt.verify');
